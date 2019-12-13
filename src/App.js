f7450557a7fdacaf93d33111fbab3ec1c3f2b1e7
mypttwo <mypt.two@gmail.com>
2019-12-13 17:57:32 +0530

    Adding basic routes for home page...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Home from './Containers/Home/home';
+import SignUp from './Containers/Signup/signup';
+import Login from './Containers/Login/login';
 
 
 function App() {
   return (
     <div className="App">
-      hello world
+     <BrowserRouter>
+     <Switch>
+       <Route exact path='/' component={Home} />
+       <Route exact path='/signup' component={SignUp} />
+       <Route exact path='/login' component={Login} />
+     </Switch>
+     </BrowserRouter>
     </div>
   );
 }
