b0f06a0c23e4c2626f338ca5d264d4cb3b2e371b
mypttwo <mypt.two@gmail.com>
2019-12-18 06:49:24 +0530

    Undoing changes to Dashboard...

--- src/Containers/Dashboard/dashboard.js
+++ src/Containers/Dashboard/dashboard.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
+const axios = require('axios');
+
+import {server} from '../../config';
+import {get_currency_market_data} from '../../ExchangeDataReader/reader';
 
 class Dashboard extends Component{
+    state = {
+        authToken : null,
+        userDbId : null,
+    }
+    componentDidMount(){
+        if(!this.props.authToken){
+            this.props.handleLogout();// this will not work if props are not populated.
+        }
+        this.setState({
+            authToken : this.props.authToken,
+            userDbId : this.props.userDbId,            
+        })
+    }
     render(){
         return <div>
             Dashboard
@@ -9,3 +26,4 @@ class Dashboard extends Component{
 }
 
 export default Dashboard;
+
