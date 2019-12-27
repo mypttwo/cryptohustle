e2cb3be2612420440cf99b718610a930d8303700
mypttwo <mypt.two@gmail.com>
2019-12-27 23:27:56 +0530

    Adding support for currency and market lists...

---
+++ src/Utils/currencyList.js
@@ -0,0 +1,6 @@
+const currencyList = {
+    eth : {name : 'Ethereum', symbol : 'ETH'},
+    dash : {name: 'Dash', symbol : 'DASH'}
+}
+
+export default currencyList;
---
+++ src/Utils/marketList.js
@@ -0,0 +1,6 @@
+const marketList = {
+    eth : {name : 'Ethereum', symbol : 'ETH'},
+    usdt : {name: 'USDT', symbol : 'USDT'}
+}
+
+export default marketList;
