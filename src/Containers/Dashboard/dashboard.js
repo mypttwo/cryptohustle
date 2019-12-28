877ee504b05984c86fbddc5a1ad578f06058f47e
mypttwo <mypt.two@gmail.com>
2019-12-28 19:43:21 +0530

    Adding Dashboard page...

--- src/Containers/Dashboard/dashboard.js
+++ src/Containers/Dashboard/dashboard.js
@@ -1,21 +1,126 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-import AuthContext from "../../authContext";
-// import {server} from '../../config';
-// import {get_currency_market_data} from '../../ExchangeDataReader/reader';
+import {server} from '../../config';
+import AuthContext from '../../authContext';
+import {withAppContext} from '../../HOC/withAppContext';
+import {getAuthConfig} from '../../Utils/getAuthConfig';
+import ControlBar from '../../Components/controlBar';
+import currencyList from '../../Utils/currencyList';
+import marketList from '../../Utils/marketList';
+import ExchangeCard from '../ExchangeCard/exchangeCard';
+import exchange from '../../ExchangeDataReader/exchange';
 
 class Dashboard extends Component{
     static contextType = AuthContext;
-
-    componentDidMount = () => {
-        console.log(this.context.userDbId);
+    state = {
+        exchanges : [],
+        currency : currencyList.eth,
+        market : marketList.usdt,
+        exchangeData : []
     }
 
-    render(){        
-        return <div>
-            Dashboard
-        </div>
+    getExchangeNameFromKey = (key) => {
+        let exchangeArr = this.props.exchanges.filter((exchange) => {
+            return exchange.key == key
+        })        
+        if(exchangeArr.length){
+            return exchangeArr[0].name;
+        }
+        return '';  
+    }
+
+    getExchangeData =  (exchanges, currency, market) => {
+        let requestedCurrency = currency ? currency:this.state.currency;
+        let requestedMarket = market ? market : this.state.market;
+        let exchangeList =  exchanges.map(async ex => {
+            let exchangeName = this.getExchangeNameFromKey(ex.exchangeKey);
+            let data = await exchange[ex.exchangeKey].read(requestedCurrency, requestedMarket);
+            return {
+                exchangeKey : ex.exchangeKey,
+                exchangeName,
+                data
+            }
+        })
+
+        Promise.all(exchangeList).then((resArray) => {
+            console.log(resArray);
+            this.setState({
+                exchangeData : resArray
+            })
+        })
+    }
+
+    getTrackedExchanges = () => {
+        let config = getAuthConfig(this.context); 
+        axios.get(`${server}/user/trackedexchanges`, config).then((res) => {
+            if(res.data){
+                this.setState({
+                    exchanges : res.data
+                })
+                this.getExchangeData(res.data)
+            }
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+
+
+
+    componentDidMount = () => {
+        this.getTrackedExchanges();        
+        
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    getExchangeCardsJSX = () => {
+        return this.state.exchangeData.map(exchange => {
+            return <ExchangeCard 
+            key={exchange.exchangeKey} 
+            currency={this.state.currency}
+            market={this.state.market}
+            exchangeData={exchange}/>
+        })
+    }
+
+    selectCurrency = (currency) => {
+        this.setState({
+            currency : currency
+        })
+        this.getExchangeData(this.state.exchanges, currency);
+    }
+
+    selectMarket = (market) => {
+        this.setState({
+            market : market
+        })
+        this.getExchangeData(this.state.exchanges, null, market);
+    }
+
+    render(){                
+        return (
+        <React.Fragment>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-sm-3 mb-2">
+                        <ControlBar selectCurrency={this.selectCurrency} selectMarket={this.selectMarket}  />
+                    </div>
+                </div>       
+                <div className="row">
+                    <div className="col-sm-12 mb-2">
+                        <h4>{this.state.currency.symbol} / {this.state.market.symbol}</h4>
+                    </div>
+                </div>       
+                <div className="row">
+                    {this.getExchangeCardsJSX()}
+                </div>
+            </div>
+        </React.Fragment>
+        )
     }
 }
 
-export default Dashboard;
+export default withAppContext(Dashboard);
---
+++ src/Containers/ExchangeCard/exchangeCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ExchangeCard = (props) => {
+
+    
+        return(
+            <div className="col-sm-3 mb-2">
+                <div className="card mb-2">
+                    <div className="card-header">{props.exchangeData.exchangeName}</div>
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Last Price : {props.exchangeData.data.lastPrice}</li>
+                    <li className="list-group-item">Buy : {props.exchangeData.data.buy}</li>
+                    <li className="list-group-item">Sell : {props.exchangeData.data.sell}</li>
+                </ul>
+                <div className="card-body">
+                    <p>
+                        <a className="btn btn-light" data-toggle="collapse" href="#collapse" role="button" aria-expanded="false" aria-controls="collapse">
+                        Show All
+                        </a>
+                    </p>
+                    <div className="collapse" id="collapse">
+                        <div className="collapse card card-body" id="collapse">
+                            {/* '<AllData data={props.exchangeData.alldata} />' */}
+                        </div>
+                    </div>   
+                </div>
+                <div className="card-footer">
+        <small className="text-muted">Last updated : {'' + new Date()}</small>
+                </div>
+            </div> 
+          </div>       
+      )            
+    
+}
+
+export default ExchangeCard
