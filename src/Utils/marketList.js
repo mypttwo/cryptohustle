ef081c4497109df1f839d1c909b427ed3088dd95
mypttwo <mypt.two@gmail.com>
2019-12-30 10:26:58 +0530

    Adding more markets ...

--- src/Utils/marketList.js
+++ src/Utils/marketList.js
@@ -1,6 +1,11 @@
 const marketList = {
     eth : {name : 'Ethereum', symbol : 'ETH'},
-    usdt : {name: 'USDT', symbol : 'USDT'}
+    usdt : {name: 'USDT', symbol : 'USDT'},
+    btc : {name : 'Bitcoin', symbol : 'BTC'},
+    xrp : {name : 'XRP', symbol : 'XRP'},
+    usdc : {name : 'USD Coin', symbol : 'USDC'},
+    trx : {name : 'Tron', symbol : 'TRX'},
+    pax : {name : 'PAX Standard', symbol : 'PAX'},
 }
 
 export default marketList;
