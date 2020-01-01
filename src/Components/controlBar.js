ce3fae0f5aa3c88a6f68a25161b5d2e72d4d7d89
mypttwo <mypt.two@gmail.com>
2020-01-01 13:31:07 +0530

    Making the list of currencies dropdown multicolumn....

--- src/Components/controlBar.js
+++ src/Components/controlBar.js
@@ -3,9 +3,18 @@ import React from 'react';
 import currencyList from '../Utils/currencyList';
 import marketList from '../Utils/marketList';
 
+const getCurrencyListJSX = (props) => {
+    return Object.values(currencyList)
+    .map(currency => {
+    return (
+    <li key={currency.symbol}>
+        <a  className="dropdown-item" href="#" 
+        onClick={() => props.selectCurrency(currency)}>{currency.symbol}</a>
+    </li>);
+    });
+}
 
 const controlBar = (props) => {
-    let clmarkup = Object.values(currencyList).map(currency => <a key={currency.symbol} className="dropdown-item" href="#" onClick={() => props.selectCurrency(currency)}>{currency.symbol}</a>);
     let mlmarkup = Object.values(marketList).map(currency => <a key={currency.symbol} className="dropdown-item" href="#" onClick={() => props.selectMarket(currency)}>{currency.symbol}</a>);
     
     return(
@@ -13,14 +22,16 @@ const controlBar = (props) => {
             <div className="col-sm-3 pt-3">
             <div className="btn-group" role="group" aria-label="Button group">
                 {/* <button type="button" className="btn btn-primary" onClick={props.loadData}>Refresh</button> */}
+                
                 <div className="btn-group" role="group">
                     <button id="btnGroupDrop1" type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" >
                     CURRENCY
                     </button>
-                    <div className="dropdown-menu">
-                    {clmarkup}
-                    </div>
+                    <ul className="dropdown-menu columns">
+                    {getCurrencyListJSX(props)}
+                    </ul>
                 </div>
+                
                 <div className="btn-group" role="group">
                     <button id="btnGroupDrop2" type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" >
                     MARKET
